545f8f12344f280d159a844243f8ac2d7edd622c
vishnu <vishnupavan777@gmail.com>
2020-12-13 22:52:51 +0530

    right shape inverted

---
+++ patterns/right_sided_inv.py
@@ -0,0 +1,19 @@
+# Printing the following pattern
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
+
+
+import os
+os.system('cls')
+
+num = int(input('Enter a positive number: '))
+
+for i in range(1, num + 1):
+    for j in range(1, i):
+        print(' ', end=' ')
+    for k in range(num + 1, i, -1):
+        print('*', end=' ')
+    print('\n')
