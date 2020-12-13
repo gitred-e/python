7fc39be63c649cbb6ca9474d7762aea5796556f7
vishnu <vishnupavan777@gmail.com>
2020-12-13 22:52:19 +0530

    right shape

---
+++ patterns/right_sided.py
@@ -0,0 +1,19 @@
+# Printing the following pattern
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+
+
+import os
+os.system('cls')
+
+num = int(input('Enter a positive number: '))
+
+for i in range(1, num+1):
+    for j in range(i, num):
+        print(' ', end=' ')
+    for k in range(1, i+1):
+        print('*', end=' ')
+    print('\n')
