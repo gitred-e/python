589e4ba7ea4c3a39edc5936495038b99469c3b1b
vishnu <vishnupavan777@gmail.com>
2020-12-13 22:45:25 +0530

    simple shape

---
+++ patterns/plane.py
@@ -0,0 +1,17 @@
+# Printing the below pattern
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+import os
+os.system('cls')
+
+
+num = int(input('Enter a positive number:'))
+
+for i in range(1, num+1):
+    for j in range(1, num+1):
+        print('*', end=' ')
+    print('\n')
