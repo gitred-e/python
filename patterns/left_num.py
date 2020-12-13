97a22708407ad5627fc66db330c4a364a87a8f56
vishnu <vishnupavan777@gmail.com>
2020-12-13 22:47:10 +0530

    left side number

---
+++ patterns/left_num.py
@@ -0,0 +1,16 @@
+# Printing the following pattern
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+import os
+os.system('cls')
+
+num = int(input('Enter a positive number: '))
+
+for i in range(1, num+1):
+    for j in range(1, i+1):
+        print(j, end=' ')
+    print('\n')
