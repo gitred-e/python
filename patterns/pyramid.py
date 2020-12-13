216c7b9ae2c7f847b823b417ab905de3585d202b
vishnu <vishnupavan777@gmail.com>
2020-12-13 22:51:01 +0530

    pyramid shape

---
+++ patterns/pyramid.py
@@ -0,0 +1,20 @@
+# Prinitng the following pattern
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+import os
+os.system('cls')
+
+num = int(input('Enter a positive number: '))
+
+for i in range(1, num+1):
+    for j in range(i, num):
+        print(' ', end=' ')
+    for k in range(1, i+1):
+        print('*', end=' ')
+    for l in range(1, i):
+        print('*', end=' ')
+    print('\n')
